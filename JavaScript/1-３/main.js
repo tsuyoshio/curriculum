25677ec1cdd413025d15ed536a7f57a864132967
tsuyoshio <99540018+tsuyoshio@users.noreply.github.com>
2022-02-20 22:48:50 +0900

    Create main.js

---
+++ JavaScript/1-３/main.js
@@ -0,0 +1,23 @@
+let a= 15
+let b=16
+if(a != 10){
+    console.log(a);
+}
+if(b >= 10 && b < 20 && b % 2 === 0){
+    console.log(b);
+}
+
+let x = 10;
+if (x >= 10 && x <=20) {
+    console.log('成功です'); 
+} else {
+    console.log('失敗です'); 
+}
+
+let y=0
+if (y % 2 === 0) {
+    console.log('偶数です');
+}
+if (y % 2 !== 0) {
+    console.log('奇数です');
+}

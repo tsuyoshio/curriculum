548540318f25d07233d147607847b6389b4d02e1
tsuyoshio <99540018+tsuyoshio@users.noreply.github.com>
2022-02-26 15:07:29 +0900

    Create main.js

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,12 @@
+let scores = [10, 15, 20, 25];
+for (let i = 0; i < scores.length; i++) {
+    if (i % 2 == 0) {
+      console.log(scores[i]+"は偶数です")
+    }
+    
+  }
+
+let car = {gass: 20.5, height: 90, weight: 200,num: 1234,};
+
+console.log("ガソリンは"+[car.gass]+"です");
+console.log("ナンバーは"+[car.num]+"です");
